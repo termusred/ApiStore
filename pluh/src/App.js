ca0803d8905d13929fac0b786fc7b5484455da34
Muslima <asimovamuslima17@gmail.com>
2024-04-30 18:51:23 +0500

    pluh

--- pluh/src/App.js
+++ pluh/src/App.js
@@ -1,9 +1,23 @@
 import Form from "./components/Form.jsx"
+import { useState } from "react";
 
 function App() {
+  const [user , setUser]  = useState([
+  {
+    username:"hacker",
+    password:"123"
+  },
+  {
+    username:"bruh",
+    password:"321"
+  }
+])
+  user.map((el) => {
+    
+  })
   return (
     <div className="App">
-      <Form/>
+      <Form />
     </div>
   );
 }
--- pluh/src/components/Form.jsx
+++ pluh/src/components/Form.jsx
@@ -1,16 +1,13 @@
 import "../styles/form.css"
 import { useState } from "react"
 
-const Form = () => {
-    const [user , setUser]  = useState("")
-    const [password , setPassword]  = useState("")
+const Form = ({}) => {
+    
     function HandleSubmit(e) {
-        setUser("")
-        setPassword("")
         e.preventDefault()
-        setUser(e.target[0].value);
-        setPassword(e.target[1].value)
-        alert("You have login as " + user)
+        // if(user.includes(e.target[0].value) && user.includes(e.target[1].value)){
+        //     alert("You have logged in as " + e.target[0].value)
+        // }
     }
 
     return (      
